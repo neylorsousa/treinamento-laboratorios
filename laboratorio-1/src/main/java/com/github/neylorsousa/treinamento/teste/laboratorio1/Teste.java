6d7b9cc95885e13cb003513e94197088a9708b12
Neylor Leandro de Sousa <neylorsousa@gmail.com>
2016-10-19 12:30:09 -0200

    Resultado final do laboratório 1.

---
+++ laboratorio-1/src/main/java/com/github/neylorsousa/treinamento/teste/laboratorio1/Teste.java
@@ -0,0 +1,12 @@
+package com.github.neylorsousa.treinamento.teste.laboratorio1;
+
+public class Teste {
+	public static void main(String[] args) {
+		CarrinhoCompra carrinho = new CarrinhoCompra();
+		carrinho.addItem(new Item("Livro", 10.0));
+		carrinho.addItem(new Item("Carrinho", 20.0));
+		carrinho.addItem(new Item("Papel", 21.0));
+
+		System.out.println(carrinho.getItens().size() == 3);
+	}
+}
