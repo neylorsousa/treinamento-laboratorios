6462237e0020b773d09312443beda17783f197d5
neylor <neylorsousa@gmail.com>
2016-10-20 18:11:41 -0200

    Feito template para testes de integração

---
+++ laboratorio-3/src/main/java/com/github/neylorsousa/treinamento/laboratorio3/Greeter.java
@@ -0,0 +1,13 @@
+package com.github.neylorsousa.treinamento.laboratorio3;
+
+import java.io.PrintStream;
+
+public class Greeter {
+	public void greet(PrintStream to, String name) {
+		to.println(createGreeting(name));
+	}
+
+	public String createGreeting(String name) {
+		return "Hello, " + name + "!";
+	}
+}
